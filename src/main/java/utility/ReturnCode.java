aee52f91fb5f3ca240958eb623d788845a5cbb59
Leo Chen <leo@leochen.i234.me>
2022-08-06 00:00:29 +0800

    1. update status code 2. add publish to nexus repo. via gradle.

--- src/main/java/utility/ReturnCode.java
+++ src/main/java/utility/ReturnCode.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 @Getter
 public enum ReturnCode implements IReturnCode{
   OK(0, "Success"),
-  ERR_DBFAIL(1000, "Database error"),
-  ERR_NETWORK(2000, "Network error");
+  ERR_DATA(1000, "Data error"),
+  ERR_DBFAIL(2000, "Database error"),
+  ERR_NETWORK(3000, "Network error"),
+  ERR_INTERNAL(4000, "Database error");
 
   private int code;
   private String msg;
